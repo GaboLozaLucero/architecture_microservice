bade875e4bedab36c40666d3d50179cbe457dc60
coriccf <corina.colque.f@gmail.com>
2022-06-01 13:16:15 -0400

    Adding ms-discovery

---
+++ ms-discovery/src/test/java/project/backend/msdiscovery/MsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package project.backend.msdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
