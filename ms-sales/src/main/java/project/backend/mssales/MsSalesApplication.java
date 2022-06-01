7b75c6f1dfff26d5d900ffbf9369c84f367f4b9a
coriccf <corina.colque.f@gmail.com>
2022-06-01 13:10:40 -0400

    Adding Eureka Client

--- ms-sales/src/main/java/project/backend/mssales/MsSalesApplication.java
+++ ms-sales/src/main/java/project/backend/mssales/MsSalesApplication.java
@@ -2,8 +2,10 @@ package project.backend.mssales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsSalesApplication {
 
 	public static void main(String[] args) {
